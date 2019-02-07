67b5980b1087679e81ffe5ac07680919480336e1
Nishant Rao <nishanthrao29@gmail.com>
2019-02-07 17:12:49 +0530

    Routes and ui changes

---
+++ src/components/Routes/Skills/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const skills = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default skills;
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,30 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Navigation.css';
 
 const Navigation = () =>{
   return(
     <div className="nav-wrapper">
-    <nav className="navbar navbar-expand-lg navbar-dark pb-0" style={{backgroundColor:'transparent'}}>
-        <a className="navbar-brand mb-0 mr-auto" href="/">NISHANT RAO</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+      <nav className="navbar navbar-expand-lg navbar-dark pb-0" style={{backgroundColor:'transparent'}}>
+        <a className="navbar-brand" href="/">Nishant Rao</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+          aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="navbar navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link"  href="/"><i class="fas fa-home" data-toggle="tooltip" data-placement="top" title="Home"></i></a>
-            </li>
-            <li className="nav-item" >
-              <a className="nav-link"  href="/Skills"><i class="fas fa-cogs" data-toggle="tooltip" data-placement="top" title="Skills"></i></a>
-            </li>
-            <li className="nav-item" >
-              <a className="nav-link"  href="/Work"><i class="fas fa-laptop-code" data-toggle="tooltip" data-placement="top" title="Work"></i></a>
-            </li>
-            <li className="nav-item" >
-              <a className="nav-link"  href="/Resume"><i class="fas fa-file-pdf" data-toggle="tooltip" data-placement="top" title="Resume"></i></a>
-            </li>
-          </ul>
-        </div>
+      <div className="collapse navbar-collapse" id="basicExampleNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link"  href="/"><i className="fas fa-home" data-toggle="tooltip" data-placement="top" title="Home"></i></a>
+          </li>
+          <li className="nav-item" >
+            <Link className="nav-link"  to="/Skills"><i className="fas fa-cogs" data-toggle="tooltip" data-placement="top" title="Skills"></i></Link>
+          </li>
+          <li className="nav-item" >
+            <a className="nav-link"  href="/Work"><i className="fas fa-laptop-code" data-toggle="tooltip" data-placement="top" title="Work"></i></a>
+          </li>
+          <li className="nav-item" >
+            <a className="nav-link"  href="/Resume"><i className="fas fa-file-pdf" data-toggle="tooltip" data-placement="top" title="Resume"></i></a>
+          </li>
+        </ul>
+      </div>
       </nav>
     </div>
   );
