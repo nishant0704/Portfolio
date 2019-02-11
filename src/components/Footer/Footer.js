78a77ea04e21f4253edba3c63bedd05cfd01e9f9
Nishant Rao <nishanthrao29@gmail.com>
2019-02-11 13:32:40 +0530

    ui changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,17 +3,13 @@ import './Footer.css';
 
 const footer = () => {
   return(
-    <div className="footer-container">
-      <div className="row m-0">
-        <div className="col-5 text-left">
-          <p>&copy;Nishant Rao</p>
-        </div>
-        <div className="text-center">
-          <a href="https://www.google.com"><i className="fab fa-linkedin social-icons"></i></a>
-          <a href="https://www.google.com"><i className="fab fa-github-square social-icons"></i></a>
-          <a href="https://www.google.com"><i className="fab fa-facebook social-icons"></i></a>
-          <a href="https://www.google.com"><i className="fab fa-instagram social-icons"></i></a>
-        </div>
+    <div className="footer-container d-inline">
+      <div className="">
+        <a href="https://www.google.com"><i className="fab fa-linkedin social-icons"></i></a>
+        <a href="https://www.google.com"><i className="fab fa-github-square social-icons"></i></a>
+        <a href="https://www.google.com"><i className="fab fa-facebook social-icons"></i></a>
+        <a href="https://www.google.com"><i className="fab fa-instagram social-icons"></i></a>
+        <span>&copy;Nishant Rao</span>
       </div>
     </div>
   );
