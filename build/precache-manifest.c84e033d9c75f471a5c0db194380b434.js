f4e82f33e4686e64fe8a841a8bb336fa6d3d0522
nishant0704 <nishanthrao29@gmail.com>
2019-05-13 21:45:32 +0530

    new changes

---
+++ build/precache-manifest.c84e033d9c75f471a5c0db194380b434.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "25e63889dbfb6aef7aea8352ef60dbae",
+    "url": "/static/media/React.25e63889.png"
+  },
+  {
+    "revision": "ecd184f919bb1b7b9fbc",
+    "url": "/static/css/main.6116a6d5.chunk.css"
+  },
+  {
+    "revision": "87736136e4f60cb28804",
+    "url": "/static/js/1.87736136.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e802338a77f34b6fa5e552bbb7a50d5",
+    "url": "/static/media/Nishant3.0e802338.jpg"
+  },
+  {
+    "revision": "3801cbe6abb44ae4709a441386e4ca52",
+    "url": "/static/media/HTML5.3801cbe6.png"
+  },
+  {
+    "revision": "8f047bb893c3c4ee94de0832935d7c89",
+    "url": "/static/media/CSS3.8f047bb8.png"
+  },
+  {
+    "revision": "afb47678875386aa6e81b65aa0640563",
+    "url": "/static/media/Javascript.afb47678.png"
+  },
+  {
+    "revision": "467a9ed981db8bc06adf79b1b500a927",
+    "url": "/static/media/MDB.467a9ed9.png"
+  },
+  {
+    "revision": "ecd184f919bb1b7b9fbc",
+    "url": "/static/js/main.ecd184f9.chunk.js"
+  },
+  {
+    "revision": "00c6e60e6f7d942826eb51df3f3958a0",
+    "url": "/static/media/Git.00c6e60e.png"
+  },
+  {
+    "revision": "bff46b9e3ee21af6a1197e0af2ed2b1c",
+    "url": "/static/media/Atom.bff46b9e.png"
+  },
+  {
+    "revision": "e8a98f8888aaef67ea1d404ffdaccb88",
+    "url": "/static/media/Chrome-Dev-Tools.e8a98f88.png"
+  },
+  {
+    "revision": "03270bbcd77388ec85f1ab7ab66d065f",
+    "url": "/static/media/ColorGuess.03270bbc.png"
+  },
+  {
+    "revision": "b5b0ea597708f09541ef79411f26a3db",
+    "url": "/static/media/GameDB.b5b0ea59.png"
+  },
+  {
+    "revision": "0e66069cb206bf92da3c4f86389c02ad",
+    "url": "/static/media/Burger-Builder.0e66069c.png"
+  },
+  {
+    "revision": "90aa626dda05a21dd3049ada59bb61cf",
+    "url": "/static/media/weather-data.90aa626d.png"
+  },
+  {
+    "revision": "377ec2ae08c8dd86bd692aef4ea54e9f",
+    "url": "/static/media/Calculator.377ec2ae.png"
+  },
+  {
+    "revision": "bd6654e037954713cd641b0dc52cbca3",
+    "url": "/index.html"
+  }
+];
